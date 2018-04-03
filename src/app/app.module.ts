d51995cd0f3f1e5b964d73ed25e1f0ad6a61efaf
Callum Chisholm <callum@techgorilla.io>
2018-04-03 12:29:30 +1000

    Material 5 Responsive Sidenav Starter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MainComponent } from './components/main/main.component';
+import {MaterialModule} from './imports/material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        MaterialModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,89 @@
+import {Component, NgZone, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+    //Sidenav responsive
+    width;
+    height;
+    mode:string = 'side';
+    open = 'true';
+    title = 'Responsive Sidenav Starter';
+    navList: NavList[];
+
+    constructor(public ngZone:NgZone,)
+    {
+        this.navList = [
+            { categoryName: 'Tab 1', icon: 'face', dropDown:false,
+                subCategory:
+                    [
+                        { subCategoryName: 'Item 1', subCategoryLink:'/link1', visable: true, },
+                        { subCategoryName: 'Item 2', subCategoryLink:'/link1', visable: true, },
+                        { subCategoryName: 'Item 3', subCategoryLink:'/link1', visable: true, },
+                    ]
+            },
+            { categoryName: 'Tab 2', icon: 'question_answer', dropDown:false,
+                subCategory:
+                    [
+                        { subCategoryName: 'Item 1', subCategoryLink:'/link1', visable: true, },
+                        { subCategoryName: 'Item 2', subCategoryLink:'/link1', visable: true, },
+                        { subCategoryName: 'Item 3', subCategoryLink:'/link1', visable: true, },
+                    ]
+            },
+            { categoryName: 'Tab 3', icon: 'work', dropDown:false,
+                subCategory:
+                    [
+                        { subCategoryName: 'Item 1', subCategoryLink:'/link1', visable: true, },
+                        { subCategoryName: 'Item 2', subCategoryLink:'/link1', visable: true, },
+                    ]
+            },
+        ];
+        this.changeMode();
+        window.onresize = (e) => {
+            ngZone.run(() => {
+                this.changeMode();
+            });
+        };
+    }
+
+    ngOnInit() {
+
+    }
+
+    changeMode() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+        if(this.width <= 800) {
+            this.mode = 'over';
+            this.open = 'false';
+        }
+        if(this.width > 800) {
+            this.mode = 'side';
+            this.open = 'true';
+        }
+    }
+
+}
+
+export class NavList {
+    categoryName: string;
+    icon: string;
+    dropDown: boolean;
+    subCategory: NavListItem[];
+    constructor(_categoryName:string,_icon:string,_dropDown:boolean,_subCategory:NavListItem[]) {
+        this.categoryName = _categoryName;
+        this.icon = _icon;
+        this.dropDown = _dropDown;
+        this.subCategory = _subCategory;
+    }
+}
+
+export class NavListItem {
+    subCategoryName: string;
+    subCategoryLink: string;
+    visable: boolean;
+}
